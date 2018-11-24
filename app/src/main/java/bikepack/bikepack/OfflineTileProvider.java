5fd3ab0904a30ec389e9cfc9648e5cd1a241e8ca
Francois Chataigner <francois.chataigner@eurecat.org>
2018-11-24 22:57:56 +0100

    optionally disable download

--- app/src/main/java/bikepack/bikepack/OfflineTileProvider.java
+++ app/src/main/java/bikepack/bikepack/OfflineTileProvider.java
@@ -24,28 +24,30 @@ public class OfflineTileProvider implements TileProvider
     private static final float TILE_STORAGE_MAX_MB = 10; // per map
 
     private final Context context;
-    private final String mapDirectoryName;
+    private final String mapName;
     private final String baseUrl;
+    private final boolean downloadIfMissing;
     private final FileStorageManager storageManager;
 
-    public OfflineTileProvider( Context context, String mapDirectoryName, String baseUrl )
+    public OfflineTileProvider( Context context, String mapName, String baseUrl, boolean downloadIfMissing )
     {
         this.context = context;
-        this.mapDirectoryName = mapDirectoryName;
+        this.mapName = mapName;
         this.baseUrl = baseUrl;
+        this.downloadIfMissing = downloadIfMissing;
 
         List<File> mapFiles = new ArrayList<>();
 
         for ( String filename : context.fileList() )
         {
-            if ( filename.startsWith(mapDirectoryName) )
+            if ( filename.startsWith(mapName) )
                 mapFiles.add( new File( context.getFilesDir(), filename ) );
         }
 
         this.storageManager = new FileStorageManager(TILE_STORAGE_MAX_MB, mapFiles );
 
         Log.i( LOG_TAG, String.format( "map='%s' files=%d size=%.2fMb",
-                mapDirectoryName,
+                mapName,
                 storageManager.getFileCount(),
                 storageManager.getSizeMb() ) );
     }
@@ -55,32 +57,37 @@ public class OfflineTileProvider implements TileProvider
     {
         try
         {
-            String tileFilename = String.format("%s-%d-%d-%d.png", mapDirectoryName, zoom, x, y );
+            String tileFilename = String.format("%s-%d-%d-%d.png", mapName, zoom, x, y );
 
             Bitmap tileBitmap = null;
             File tileFile = context.getFileStreamPath(tileFilename);
 
             if ( tileFile.exists() )
             {
-                Log.i( LOG_TAG, "loading existing tile="+tileFilename );
                 FileInputStream fileStream = context.openFileInput(tileFilename);
                 tileBitmap = BitmapFactory.decodeStream(fileStream);
+                Log.i( LOG_TAG, String.format("loading existing %dx%d tile=%s size=%dKb",
+                        tileBitmap.getWidth(),
+                        tileBitmap.getHeight(),
+                        tileFilename,
+                        Math.round(tileFile.length()/1000) ) );
             }
-            else // download tile
+            else if (downloadIfMissing) // download tile
             {
                 String tileUrl = baseUrl
                     .replace("{zoom}", ""+zoom)
                     .replace("{x}",""+x)
                     .replace("{y}",""+y);
 
-                Log.i( LOG_TAG, "retrieving tile="+tileUrl );
+                Log.i( LOG_TAG, "retrieving="+tileUrl );
                 tileBitmap = Picasso.get().load(tileUrl).get();
 
-                Log.i( LOG_TAG, "saving file="+tileFilename );
+                Log.i( LOG_TAG, "saving="+tileFilename );
                 FileOutputStream fileStream = context.openFileOutput( tileFilename, Context.MODE_PRIVATE );
                 tileBitmap.compress(Bitmap.CompressFormat.PNG, 100, fileStream);
                 storageManager.add(tileFile);
             }
+            else return null;
 
             ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
             tileBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
